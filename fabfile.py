6ec6b651d7bbf1fc074f10b2a9cd97a555ba919f
Che-Wei Lin <linton.tw@gmail.com>
2015-06-10 23:39:29 +0800

    Add fabric for fast deploy

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import task, local
+
+
+@task
+def snort_unsock():
+    local('snort -i em1 -A unsock -l /tmp -c /etc/snort/etc/snort.conf')
+
+
+@task
+def snort_console():
+    local('snort -i em1 -c /etc/snort/etc/snort.conf -A console')
+
+
+@task
+def pigrelay():
+    local('python pigrelay.py &')
+
+
+@task
+def clean():
+    local('rm -rf /tmp/*')
